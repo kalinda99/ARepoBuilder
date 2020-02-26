7136c2be6e301726a6101239e817096659ed93a8
Holly <kalinda@andontie.net>
2020-02-26 12:57:19 -0500

    Commented out set -x, don't need it uncommented

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 pkgname=$(basename "$PWD") # Defines the current directory name without path, which is the name of the pkg
-set -x # Stops script if any errors are encountered
+# set -x # Stops script if any errors are encountered
 
 # checkpkg
 git clean -fxd
